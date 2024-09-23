2287937e3826dfbe7fc6bc1e8540a38c7f50d30f
mtuan120304 <mtuan120304@gmail.com>
2024-09-24 01:37:27 +0700

    feat:contact-page, order-page, revenue-page

---
+++ app/order/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import OrderPageComponent from '../../components/order-page/OrderDelivery/OrderDelivery'
+export default function Order() {
+  return (
+    <div>
+        <OrderPageComponent/>
+    </div>
+  )
+}
