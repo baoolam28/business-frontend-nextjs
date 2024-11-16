cc4e4e17344e41c97f18a3ff4e991ca9c230196d
NguyenGiaHuy14092004 <huynguyen140904@gmail.com>
2024-11-16 08:37:41 +0700

    feat: report

--- app/report/page.js
+++ app/report/page.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReportDashBoard from '../../components/component/report'
+import ReportDashBoard from '../../components/component/revenue-dashboard'
 export default function page() {
   return (
     <div>
